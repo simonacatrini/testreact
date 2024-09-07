25f8228deac3fa2bca3aaa5c07f74c28bd1d0906
simonacatrini <simoacatrini12@yahoo.com>
2024-09-07 20:58:32 +0300

    c4

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Profile.module.css';
+import './Profile.css';
 
 function Profile({ username, tag, location, avatar, stats }) {
   return (
-    <div className={styles.profile}>
-      <div className={styles.description}>
-        <img src={avatar} alt="User avatar" className={styles.avatar} />
-        <p className={styles.name}>{username}</p>
-        <p className={styles.tag}>@{tag}</p>
-        <p className={styles.location}>{location}</p>
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
-      <ul className={styles.stats}>
+      <ul className="stats">
         <li>
-          <span className={styles.label}>Followers: </span>
-          <span className={styles.quantity}>{stats.followers}</span>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
-          <span className={styles.label}>Views: </span>
-          <span className={styles.quantity}>{stats.views}</span>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
-          <span className={styles.label}>Likes: </span>
-          <span className={styles.quantity}>{stats.likes}</span>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
